35191ab807d3e527b99feb71ab0677a0490769a4
IDStack <hakimsembiring@outlook.co.id>
2019-05-18 15:41:00 +0700

    Product: Search + Reset

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -27,6 +27,30 @@ class Product extends MY_Controller
 		$this->view($data);
 	}
 
+	public function search($page = null)
+	{
+		if (isset($_POST['keyword'])) {
+			$this->session->set_userdata('keyword', $this->input->post('keyword'));
+		} else {
+			redirect(base_url('category'));
+		}
+		$keyword 			= $this->session->userdata('keyword'); 
+		$data['title']		= 'Product';
+		$data['content']	= $this->product->select(
+								['product.id', 'product.title', 'product.image', 'category.title AS category_title']
+							)->join('category')->like('product.title', $keyword)->paginate($page)->get();
+		$data['total_rows']	= $this->product->like('title', $keyword)->count();
+		$data['pagination']	= $this->product->makePagination(site_url('product'), 3, $data['total_rows']);
+		$data['page']		= 'pages/product/index';
+		$this->view($data);
+	}
+
+	public function reset()
+	{
+		$this->session->unset_userdata('keyword');
+		redirect(base_url('product'));
+	}
+
 }
 
 /* End of file Product.php */
